b3a9190407ff3dfeabf1eb3a49d0b57f875fa157
Jozef Pohorelec <pohorelec@comvai.com>
2017-08-03 09:13:41 +0200

    added Serializable to MigrationSetKindOperations

--- src/main/java/org/ctoolkit/agent/resource/MigrationSetKindOperations.java
+++ src/main/java/org/ctoolkit/agent/resource/MigrationSetKindOperations.java
@@ -3,6 +3,7 @@ package org.ctoolkit.agent.resource;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
  */
 @XmlAccessorType( XmlAccessType.FIELD )
 public class MigrationSetKindOperations
+        implements Serializable
 {
     @XmlElement( name = "add" )
     private List<MigrationSetKindOperationAdd> add;
