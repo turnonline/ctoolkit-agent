b3f2db26b801f44ec3f94ee88132795f722db168
Jozef Pohorelec <pohorelec@comvai.com>
2018-08-20 21:17:05 +0200

    implemented unit test for blob transformer #CTA-60

---
+++ ctoolkit-agent-core/src/test/java/org/ctoolkit/agent/transformer/BlobTransformerProcessorTest.java
@@ -0,0 +1,58 @@
+package org.ctoolkit.agent.transformer;
+
+import org.ctoolkit.agent.model.api.MigrationSetPropertyBlobTransformer;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+import java.sql.Blob;
+import java.sql.Clob;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit test for {@link BlobTransformerProcessor}
+ *
+ * @author <a href="mailto:pohorelec@turnonlie.biz">Jozef Pohorelec</a>
+ */
+public class BlobTransformerProcessorTest
+{
+    private BlobTransformerProcessor processor = new BlobTransformerProcessor();
+
+    @Test
+    public void transform_ByteArrayToString()
+    {
+        byte[] data = new byte[]{'J', 'o', 'h', 'n'};
+        assertEquals( "John", processor.transform( data, new MigrationSetPropertyBlobTransformer() ) );
+    }
+
+    @Test
+    public void transform_ByteArrayToStringAsBase64()
+    {
+        byte[] data = new byte[]{'J', 'o', 'h', 'n'};
+        MigrationSetPropertyBlobTransformer transformer = new MigrationSetPropertyBlobTransformer();
+        transformer.setEncodeToBase64( true );
+
+        assertEquals( "Sm9obg==", processor.transform( data, transformer ) );
+    }
+
+    @Test
+    public void transform_BlobToString() throws Exception
+    {
+        Blob blob = mock( Blob.class );
+        when( blob.getBinaryStream() ).thenReturn( new ByteArrayInputStream( new byte[]{'J', 'o', 'h', 'n'} ) );
+
+        assertEquals( "John", processor.transform( blob, new MigrationSetPropertyBlobTransformer() ) );
+    }
+
+    @Test
+    public void transform_ClobToString() throws Exception
+    {
+        Clob clob = mock( Clob.class );
+        when( clob.getCharacterStream() ).thenReturn( new StringReader( "John" ) );
+
+        assertEquals( "John", processor.transform( clob, new MigrationSetPropertyBlobTransformer() ) );
+    }
+}
